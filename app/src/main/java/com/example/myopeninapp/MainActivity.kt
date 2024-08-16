c261986cac18c435620d0fe9dfa15eb916702aec
Anchana <anchanareghu20@gmail.com>
2024-08-16 13:53:14 +0530

    Commit to main

--- app/src/main/java/com/example/myopeninapp/MainActivity.kt
+++ app/src/main/java/com/example/myopeninapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myopeninapp.ui.components.navigation
+package com.example.myopeninapp.com.example.myopeninapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.myopeninapp.ui.components.navigation.MainScreen
 import com.example.myopeninapp.ui.theme.MyOpenInAppTheme
 
 class MainActivity : ComponentActivity() {
